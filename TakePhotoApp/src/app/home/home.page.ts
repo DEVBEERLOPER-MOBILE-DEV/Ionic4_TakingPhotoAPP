bdb7fd48ba6ce5e769b0d836209b021cfe648b19
Caivan <caivan2@gmail.com>
2019-09-13 21:27:08 -0500

    Adding plugin an functionality to take the photo

--- TakePhotoApp/src/app/home/home.page.ts
+++ TakePhotoApp/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  imageTaken: any; 
 
+  constructor(public alertController:AlertController, private camera: Camera) {}
+  
+  
+  takePhoto() {
+
+    // Options of the image taken 
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    // Invoke the camera to take the picture
+    this.camera.getPicture(options).then((imageData) => {
+
+      // Asign the image taken to the image attribute
+      this.imageTaken = 'data:image/jpeg;base64,' + imageData;
+      
+     }, (err) => {
+       // Handle error
+        this.presentAlertError ();
+     });
+
+  }
+
+  /**
+   * Present a box dialog to inform about a problem with the action of taking an image
+   */
+  async presentAlertError (){
+    const alert = await this.alertController.create({
+      header: 'ERROR',
+      subHeader: 'Image error',
+      message: 'There was an error taking the photo.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
